f8ca30d9394585e41be00ee42bee65bb3923f197
Ari Munandar <arimunandar.dev@gmail.com>
2020-03-03 11:51:27 +0700

    Update

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "OVOCommon",
-            dependencies: []),
+            dependencies: [],
+            path: "OVOCommon"),
         .testTarget(
             name: "OVOCommonTests",
             dependencies: ["OVOCommon"]),
