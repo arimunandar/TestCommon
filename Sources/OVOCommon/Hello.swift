51cef9394e62bc5e01756012053eaeec1537e1cd
Ari Munandar <arimunandar.dev@gmail.com>
2020-03-03 11:24:09 +0700

    Add files via upload

---
+++ Sources/OVOCommon/Hello.swift
@@ -0,0 +1,17 @@
+//
+//  Hello.swift
+//  OVOCommon
+//
+//  Created by OVO on 03/03/20.
+//  Copyright © 2020 Artdev Indonesia. All rights reserved.
+//
+
+import Foundation
+
+public class Hello {
+    public init() {}
+
+    public func name() -> String {
+        return "Ari Munandar"
+    }
+}
